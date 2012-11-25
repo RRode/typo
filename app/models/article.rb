03579a53ce182e3866a246acf0ec6dea6933e35f
Rok Rode <rok.rode@gmail.com>
2012-11-25 02:07:11 -0800

    Comments fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -83,7 +83,10 @@ class Article < Content
   def merge_with(other_article_id)
     target = Article.find_by_id other_article_id
     self.body = self.body + "\n" + target.body
+    #target.comments.each {|c| self.comments << c}
+    self.comments.concat target.comments
     self.save!
+    target.comments(true) #update comments cache, should now be empty
     target.destroy
     self
   end
